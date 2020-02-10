dce7bc733e4a3bae49ecd1281c846a02462407a1
cod3breaker <ravisherath@gmail.com>
2020-02-11 06:40:25 +1100

    setting up the own module

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="nester",
+    version="1.0.0",
+    py_modules=["nester"],
+    author="Sunimal Herath - hfpython",
+    author_email="sunimal**@gmail.com",
+    url="",
+    description="A simple printer of nested lists",
+)
